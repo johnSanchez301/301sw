17e165f261fe42986fb552471edcb44113a102a9
johnSanchez301 <john@301creativastudio.com>
2017-02-08 07:24:21 -0500

    estilos hover en el sitio

--- wp-content/themes/301_creativa/section-lastedpost.php
+++ wp-content/themes/301_creativa/section-lastedpost.php
@@ -6,21 +6,21 @@
     <section id="section-blog-301" class="section-blog-301">
        <article>
             <div class="container">
-                <div class="title">
+                <div class="title wow bounceInDown" data-wow-duration="2s">
                     <div class="cont-text">
                         <p>Así</p>
                         <span class="text-1">vivimos</span>
                         <span class="text-2">las calles</span>
                     </div>
                 </div>
-                <div class="text-descript"><p>Contando nuestras historias desde lo cotidiano...</p>
+                <div class="text-descript wow bounceInRight"  data-wow-duration="1s" data-wow-delay="1s"><p>Contando nuestras historias desde lo cotidiano...</p>
                 <a href="blog">Ver más...</a>
                 </div>
                 <div class="silla-301">
-                    <img src="wp-content/themes/301_creativa/assets/images/silla-y-periodico-blog.png" alt="">
+                    <img src="wp-content/themes/301_creativa/assets/images/silla-y-periodico-blog.png" alt="" class="wow bounceInLeft"  data-wow-duration="2s">
                 </div>
             </div>
-            <div class="farol-301"><img src="wp-content/themes/301_creativa/assets/images/lampara-blog.png" alt=""></div>
+            <div class="farol-301"><img src="wp-content/themes/301_creativa/assets/images/lampara-blog.png" alt="" class="wow bounceInRight" data-wow-duration="1s" data-wow-delay="1s"></div>
         </article>
     </section>
 <?php endif; ?>
