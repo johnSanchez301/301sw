5ef3b8623b0d8a8da5f572d7689f5fc36b3d1329
johnSanchez301 <john@301creativastudio.com>
2017-02-13 22:18:38 -0500

    cambios generales del template

--- wp-content/themes/301_creativa/section-lastedpost.php
+++ wp-content/themes/301_creativa/section-lastedpost.php
@@ -6,21 +6,21 @@
     <section id="section-blog-301" class="section-blog-301">
        <article>
             <div class="container">
-                <div class="title wow bounceInDown" data-wow-duration="2s">
+                <div class="title wow bounceInDown">
                     <div class="cont-text">
                         <p>Así</p>
                         <span class="text-1">vivimos</span>
                         <span class="text-2">las calles</span>
                     </div>
                 </div>
-                <div class="text-descript wow bounceInRight"  data-wow-duration="1s" data-wow-delay="1s"><p>Contando nuestras historias desde lo cotidiano...</p>
+                <div class="text-descript wow bounceInRight"><p>Contando nuestras historias desde lo cotidiano...</p>
                 <a href="blog">Ver más...</a>
                 </div>
                 <div class="silla-301">
-                    <img src="wp-content/themes/301_creativa/assets/images/silla-y-periodico-blog.png" alt="" class="wow bounceInLeft"  data-wow-duration="2s">
+                    <img src="wp-content/themes/301_creativa/assets/images/silla-y-periodico-blog.png" alt="" class="wow bounceInLeft">
                 </div>
             </div>
-            <div class="farol-301"><img src="wp-content/themes/301_creativa/assets/images/lampara-blog.png" alt="" class="wow bounceInRight" data-wow-duration="1s" data-wow-delay="1s"></div>
+            <div class="farol-301"><img src="wp-content/themes/301_creativa/assets/images/lampara-blog.png" alt="" class="wow bounceInRight"></div>
         </article>
     </section>
 <?php endif; ?>
